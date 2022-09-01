321ee7455f98243a407db55634fbe093c8fdd030
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-09-01 12:02:33 +0600

    feat: update test according
    - add tests for checking validity and everything

--- test/test.cjs
+++ test/test.cjs
@@ -1,17 +1,27 @@
 const Cache = require('../lib/index.js');
-
-const cache = new Cache(5);
+console.log('Testing commonjs');
+const cache = new Cache(4);
 
 // Adding an entry
 cache.add('key', 'value');
+cache.add('key 2', 'value 2', 10);
 
-// Make sure it returns undefined after expired time
+// Make sure key returns undefined 
+// and key2 returns value2
 setTimeout(() => {
-    console.log(cache.fetch('key'));
+    const key1 = cache.fetch('key'), key2 = cache.fetch('key 2');
+    console.log('value 1 after 5 seconds -', key1);
+    console.log('value 2 after 5 seconds -', key2);
+    if (key1 !== undefined && key2 === undefined) throw new Error('Error, key 1 should be undefined and key 2 should be valid');
+    setTimeout(() => {
+        const key22 = cache.fetch('key 2');
+        console.log('value 2 after another 5 seconds -', key22);
+        if (key22 !== undefined) throw new Error('Error, key 2 should be undefined at this point');
+    }, 6 * 1000);
 }, 5 * 1000);
 
 // Fetching an entry
-console.log(cache.fetch('key'));
+console.log('value 1 initially -', cache.fetch('key'),'\nvalue 2 initially -', cache.fetch('key 2'));
 
 // toArray and toMap test
-console.log(cache.toArray(), cache.toMap());
+console.log('Array -', cache.toArray(),'\nMap -', cache.toMap());
